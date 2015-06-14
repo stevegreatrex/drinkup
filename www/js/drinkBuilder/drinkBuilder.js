59cb4ed006b3df465c369de95529c58578acf30c
Steve Greatrex <stephen.greatrex@googlemail.com>
2015-06-14 17:02:20 +0100

    Various improvements to drink editing

--- www/js/drinkBuilder/drinkBuilder.js
+++ www/js/drinkBuilder/drinkBuilder.js
@@ -7,16 +7,21 @@
 		.controller('DrinkBuilderCtrl', function ($scope, $state, $stateParams, $ionicHistory, drinkCategories, drinkTypesRepository) {
 			$scope.selectCategory = function (category) {
 				$scope.drinkType.category = category;
-				$scope.drinkType.abv      = category.typicalAbv;
-				$scope.drinkType.calPerMl = category.typicalCalPerMl;
+				if (!$scope.drinkType.abv) {
+					$scope.drinkType.abv = drinkCategories[category].typicalAbv;
+				}
+				if (!$scope.calories.per100) {
+					$scope.calories.per100 = drinkCategories[category].typicalCalPerMl * 100;
+				}
 			};
 
 			$scope.saveDrink = function () {
+				$scope.drinkType.calPerMl = $scope.calories.per100 / 100;
 				drinkTypesRepository.save($scope.drinkType);
 				$scope.cancel();
 			};
 
-			$scope.cancel = function() {
+			$scope.cancel = function () {
 				if ($ionicHistory.backView()) {
 					$ionicHistory.goBack();
 				} else {
@@ -24,11 +29,26 @@
 				}
 			};
 
+			$scope.$on('$stateChangeSuccess', function (event, toState) {
+				if (toState.name === 'app.drink') {
+					load();
+				}
+			});
+
+			function load() {
+				$scope.drinkType = drinkTypesRepository.get($stateParams.drinkId) || {};
+
+				$scope.calories = {
+					per100: $scope.drinkType.calPerMl ? $scope.drinkType.calPerMl * 100 : null
+				};
+
+				if (!$scope.drinkType.category) {
+					$scope.selectCategory($scope.drinkCategories[0]);
+				}
+			}
+
 			$scope.drinkCategories = Object.keys(drinkCategories);
 
-			$scope.drinkType = drinkTypesRepository.get($stateParams.drinkId) || {
-					name: 'My New Drink',
-					category: $scope.drinkCategories[0]
-				};
+			load();
 		});
 }(angular));
